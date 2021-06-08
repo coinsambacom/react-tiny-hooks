e9571a75b2007a8d20e62cba049e2d5395ba85eb
Gustavo <gustavo@obex.pw>
2021-06-08 09:39:35 -0300

    added two new hooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { default as useDebounce } from './useDebounce';
 export { default as useDeviceDetect } from './useDeviceDetect';
+export { default as useInterval } from './useInterval';
 export { default as useIsClient } from './useIsClient';
 export { default as useLocalStorage } from './useLocalStorage';
+export { default as useWindowSize } from './useWindowSize';
